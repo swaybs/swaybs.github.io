66c0cd36a4b5760652aac6f90d44f93cc6d4bb46
swaybs <37590406+swaybs@users.noreply.github.com>
2018-04-30 11:46:43 -0400

    Added Nav

--- javascript.js
+++ javascript.js
@@ -1,8 +1,11 @@
-$("nav ul li").click(function(){
-  var xcoord = $(this).data("xcoord");
+/* Set the width of the side navigation to 250px and the left margin of the page content to 250px */
+function openNav() {
+    document.getElementById("mySidenav").style.width = "250px";
+    document.getElementById("main").style.marginLeft = "250px";
+}
 
-  $("nav div").stop().animate({marginLeft:xcoord}, 500, "easeInOutExpo");
-  $(this).addClass("active");
-  $("nav ul li").not(this).removeClass("active");
-
-});
+/* Set the width of the side navigation to 0 and the left margin of the page content to 0 */
+function closeNav() {
+    document.getElementById("mySidenav").style.width = "0";
+    document.getElementById("main").style.marginLeft = "0";
+}
