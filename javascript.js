ff35c6ee7f03c34c5acd904181046baf0badfb53
swaybs <37590406+swaybs@users.noreply.github.com>
2018-05-17 10:04:17 -0400

    More mobile styling

--- javascript.js
+++ javascript.js
@@ -1,21 +1,7 @@
-<<<<<<< HEAD
 
-
-$(document).ready(function(){
-  $("#header").fadein(4000)
-
-});
-=======
-// $(document).ready(function(){
-//
-//   $(".logo").fadeIn(slow,callback);
-//
-// });
 
 
 // $(document).ready(function(){
-//     $(".dropbtn").click(function(){
-//         $(".logo, .jobs ").fadeToggle("slow");
-//     });
+//   $("#header").fadein(4000)
+//
 // });
->>>>>>> 209ccac2684d564dc041f314c407e49df401bdc9
