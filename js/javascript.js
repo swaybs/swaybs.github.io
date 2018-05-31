b243702dfedbf9bcba9f679ff3f9785d22b5db7b
swaybs <37590406+swaybs@users.noreply.github.com>
2018-05-31 10:49:17 -0400

    More styles

--- js/javascript.js
+++ js/javascript.js
@@ -1,3 +1,15 @@
-// $("button").click(function(){
-//     $(".logo").fadeIn("slow");
-// });
+
+
+var enterName = prompt('Enter your name.');
+var enterPassword = prompt('Enter your password.');
+
+var name = 'Andrew';
+var password = 'Hello';
+
+
+  if ( enterName === name, enterPassword === password ){
+    alert('Welcome back ' + name + '!');
+} else {
+  alert('System will self destruct in 5...4...3...2...1....');
+
+}
