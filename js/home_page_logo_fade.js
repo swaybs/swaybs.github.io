59779011941bf1dd6fba29aaccd599ceff54784a
Andrew Rothbauer <andrew.rothbauer@gmail.com>
2018-06-27 19:43:35 -0400

    Changed up the landing page.

--- js/home_page_logo_fade.js
+++ js/home_page_logo_fade.js
@@ -1,22 +1,46 @@
-
-
 // $( document.body ).click(function() {
 //   $( "div:hidden:first" ).fadeIn(2000);
 // });
 
 
-$(document).ready(function () {
+// $(document).ready(function() {
+//   $('.fa.hidden').addClass('social_show').animate({
+//     width: '100px',
+//
+//   });
+//
+//
+//
+//
+// });
+$(document).ready(function(){
 
-    $('.fa.hidden').addClass('social_show').animate({
-      width: '100px',
-      color: 'purple'
+/*! Fades in page on load */
+$('html').css('display', 'none');
+$('html').fadeIn(3500);
 
+});
 
+// Box shadow effect with OkShadow //
 
+$('.box-shadow').okshadow({
+  color: '#555',
+  textShadow: false,
+  xMax: 10,
+  yMax: 30,
+  yOffset: 5,
+  fuzzMin: 1,
+  fuzzMax: 10,
+});
 
-    });
+// Text shadow effect with OkShadow //
 
-
-
-    // $('#header').slideDown(3000);
+$('.enter-page-logo').okshadow({
+  color: '#555',
+  textShadow: true,
+  xMax: 10,
+  yMax: 30,
+  yOffset: 5,
+  fuzzMin: 1,
+  fuzzMax: 10,
 });
