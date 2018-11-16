dc8aaa12b6553cc78e7b244cd54c143e3d6ff03e
Andrew Rothbauer <andrew.rothbauer@gmail.com>
2018-11-16 13:07:31 -0500

    implement new navbar

--- js/javascript.js
+++ js/javascript.js
@@ -1,16 +1,16 @@
 $(document).ready(function(){
 
 	// hide .navbar first
-$(".top-nav").hide();
+$(".navbar").hide();
 
 	// fade in .navbar
 $(function () {
 	$(window).scroll(function () {
             // set distance user needs to scroll before we fadeIn navbar
 		if ($(this).scrollTop() > 100) {
-			$('.top-nav').fadeIn();
+			$('.navbar').fadeIn();
 		  } else {
-			$('.top-nav').fadeOut();
+			$('.navbar').fadeOut();
 			       }
 		  });
 
